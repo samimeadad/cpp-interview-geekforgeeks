2654e3c67f1b6424b1f17f83dc6ee88d2900aedb
Sami Meadad Choudhury <sami.meadad@gmail.com>
2025-06-27 11:11:49 +0600

    first commit and first program updated

---
+++ positive-negative.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+   int x;
+   cout<<"Enter the number: ";
+   cin>>x;
+   if(x%2==0)
+    cout<<"The number is positive"<<endl;
+   else
+    cout<<"The number is negative"<<endl;
+   return 0;
+}
